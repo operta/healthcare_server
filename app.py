66f442311ddac0eceb3cf3b2a5a38d976cda1c66
Dzan Operta <dzanoperta@yahoo.com>
2020-05-30 15:49:10 +0200

    added endpoints foor doctor report and test simualtion

--- app.py
+++ app.py
@@ -1,25 +1,132 @@
 from flask import Flask, request, jsonify, make_response
 from flask_mail import Mail, Message
-from flask_cors import CORS
+from flask_cors import CORS, cross_origin
 from flask_sqlalchemy import SQLAlchemy
 import sqlite3
 from sqlite3 import Error
-import json
+import simplejson as json
+import random
 
 
 app = Flask(__name__)
 CORS(app)
+
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///healthcare.db'
 db = SQLAlchemy(app)
 
 
 
-@app.route('/pending-requests')
+class PatientRequest(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    patient_email = db.Column(db.String, nullable=False)
+    patient_id = db.Column(db.String)
+    doctor_comment = db.Column(db.String)
+    patient_comment = db.Column(db.String)
+    is_closed = db.Column(db.Boolean, default=False)
+    is_positive = db.Column(db.Boolean, default=False)
+    is_suspect = db.Column(db.Boolean, default=False)
+    temperature = db.Column(db.Numeric, nullable=False)
+    has_cough = db.Column(db.Boolean, nullable=False)
+    has_fever = db.Column(db.Boolean, nullable=False)
+    has_shortness_of_breath = db.Column(db.Boolean, nullable=False)
+    has_muscle_pain = db.Column(db.Boolean, nullable=False)
+    has_sore_throat = db.Column(db.Boolean, nullable=False)
+    has_loss_of_ts = db.Column(db.Boolean, nullable=False)
+    has_contact_with_coronac = db.Column(db.Boolean, nullable=False)
+    has_recommendation = db.Column(db.Boolean)
+
+    @property
+    def serialize(self):
+        """Return object data in easily serializable format"""
+        return {
+            'id': self.id,
+            'patient_email': self.patient_email,
+            'patient_id': self.patient_id,
+            'doctorComment': self.doctor_comment,
+            'patientComment': self.patient_comment,
+            'isClosed': self.is_closed,
+            'isPositive': self.is_positive,
+            'isSuspect': self.is_suspect,
+            'temperature': json.dumps(self.temperature),
+            'hasCough': self.has_cough,
+            'hasFever': self.has_fever,
+            'hasRecommendation': self.has_recommendation,
+            'hasShortnessOfBreath': self.has_shortness_of_breath,
+            'hasMusclePain': self.has_muscle_pain,
+            'hasSoreThroat': self.has_sore_throat,
+            'hasLossOfTasteOrSmell': self.has_loss_of_ts,
+            'hasContactWithCoronaCase': self.has_contact_with_coronac
+        }
+
+    def is_corona_suspect(self):
+        if (self.temperature > 38) and  (self.has_cough or self.has_sore_throat or self.has_muscle_pain or self.has_shortness_of_breath or self.has_contact_with_coronac or self.has_loss_of_ts):
+            return True
+        if self.has_contact_with_coronac and (self.has_cough or self.has_sore_throat or self.has_muscle_pain or self.has_shortness_of_breath or self.has_contact_with_coronac or self.has_loss_of_ts or self.has_fever):
+            return True
+
+        positive_suspections = 0
+
+        if self.has_fever: positive_suspections += 1
+        if self.has_cough: positive_suspections += 1
+        if self.has_shortness_of_breath: positive_suspections += 1
+        if self.has_muscle_pain: positive_suspections += 1
+        if self.has_sore_throat: positive_suspections += 1
+        if self.has_loss_of_ts: positive_suspections += 1
+
+        if positive_suspections > 2:
+            return True
+
+        return False
+
+
+@app.route('/requests')
+@cross_origin()
 def get_pending_requests():
-    with sqlite3.connect(DATABASE) as conn:
-        cursor = conn.cursor()
-        cursor.execute("SELECT * FROM requests WHERE isClosed=0;")
-        data = cursor.fetchall()
-        return json.dumps(data)
+    list = []
+    for r in db.session.query(PatientRequest).all():
+        list.append(r.serialize)
+
+    return make_response(jsonify(list), 200)
+
+#TODO Zana Begoli part
+@app.route('/doctor-report/<int:request_id>/<string:message>', methods=['PUT'])
+@cross_origin()
+def doctor_report(request_id, message):
+    row = db.session.query(PatientRequest).filter_by(id=request_id).first()
+    row.doctor_comment = message
+    db.session.commit()
+
+    #send message to patient
+    send_email(message, recipient=row.patient_email)
+    return make_response(jsonify({'success': request_id}), 200)
+
+
+@app.route('/close-request/<int:request_id>', methods=['PUT'])
+@cross_origin()
+def close_request(request_id):
+    row = db.session.query(PatientRequest).filter_by(id=request_id).first()
+    row.is_closed = True
+    row.is_suspect = False
+    db.session.commit()
+    return make_response(jsonify({'success': request_id}), 200)
+
+
+
+# TODO Sarah liedermans part
+@app.route('/simulate-test/<int:request_id>', methods=['PUT'])
+@cross_origin()
+def simulate_test(request_id):
+    simulated_val = bool(random.getrandbits(1))
+    row = db.session.query(PatientRequest).filter_by(id=request_id).first()
+    row.is_positive = simulated_val
+    row.is_suspect = False
+    row.is_closed = True
+    db.session.commit()
+    if not simulated_val:
+        send_email("Lab test results are negative. You don't have corona virus!", row.patient_email)
+    return make_response(jsonify({'success': 'Test done'}), 200)
 
 
 @app.route('/', methods=['POST'])
@@ -47,36 +154,44 @@ def prescreening_request():
             return make_response(jsonify({'error': 'Field hasLossOfTasteOrSmell does not exist'}), 400)
 
         patient_request = PatientRequest(
-            data['patient_email'],
-            data['temperature'],
-            data['hasCough'],
-            data['hasFever'],
-            data['hasShortnessOfBreath'],
-            data['hasMusclePain'],
-            data['hasSoreThroat'],
-            data['hasLossOfTasteOrSmell'],
-            data['hasContactWithCoronaCase']
+            patient_email=data['patient_email'],
+            patient_comment=data['patient_comment'],
+            doctor_comment=data['doctor_comment'],
+            temperature=data['temperature'],
+            has_cough=data['hasCough'],
+            has_fever=data['hasFever'],
+            has_shortness_of_breath=data['hasShortnessOfBreath'],
+            has_sore_throat=data['hasSoreThroat'],
+            has_muscle_pain=data['hasMusclePain'],
+            has_loss_of_ts=data['hasLossOfTasteOrSmell'],
+            has_contact_with_coronac=data['hasContactWithCoronaCase'],
         )
 
         conn = connect_to_db()
         create_requests_table(conn)
+        db.create_all()
 
         if patient_request.is_corona_suspect():
             msg = 'You have been automatically pre-screened as possible corona suspect.' \
                   ' Request has been sent to medical team. You will be contacted shortly.'
-            send_email(msg, patient_request.patient_email)
-            insert_request(patient_request)
+            # send_email(msg, patient_request.patient_email)
+            patient_request.is_suspect = True
+            db.session.add(patient_request)
+            db.session.commit()
             return make_response(jsonify({'message': msg}), 200)
 
         msg = "Pre-screening results are negative. You are not a corona suspect!"
-        send_email(msg, patient_request.patient_email)
-        patient_request.isNegative = True
-        insert_request(patient_request)
+        # send_email(msg, patient_request.patient_email)
+        patient_request.is_closed = True
+        db.session.add(patient_request)
+        db.session.commit()
         return make_response(
             jsonify({'message': 'Pre-screening results are negative. You are not a corona suspect!'}), 200)
 
     return make_response(jsonify({'error': 'Incorrect format, send JSON'}), 400)
 
+
+### MAIL SERVER
 
 app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
 app.config['MAIL_PORT'] = 465
@@ -94,13 +209,12 @@ def send_email(message, recipient="dzanoperta2016@gmail.com"):
     mail.send(msg)
 
 
-DATABASE = 'pythonsqlite.db'
 
-
+######## SQLITE
 def connect_to_db():
     conn = None
     try:
-        conn = sqlite3.connect(DATABASE)
+        conn = sqlite3.connect('healthcare.db')
     except Error as e:
         app.logger.error(e)
     finally:
@@ -109,41 +223,24 @@ def connect_to_db():
     return conn
 
 
-def insert_request(request):
-    with sqlite3.connect(DATABASE) as conn:
-        cur = conn.cursor()
-        create_requests_table(conn)
-        sql = """INSERT INTO requests(patient_email, temperature, hasCough, hasFever,
-                    hasShortnessOfBreath, hasMusclePain, hasSoreThroat, hasLossOfTasteOrSmell, hasContactWithCoronaCase, isClosed)
-                    VALUES(?,?,?,?,?,?,?,?,?,?)"""
-        cur = conn.cursor()
-        cur.execute(sql, request)
-        return cur.lastrowid
-
-
-
-def update_request(conn, request):
-    # TODO
-    return None
-
-
-
-
-
 def create_requests_table(conn):
-    sql = """CREATE TABLE IF NOT EXISTS requests (
+    sql = """CREATE TABLE IF NOT EXISTS patient_requests (
                 id integer PRIMARY KEY,
-                patient_email text NOT NULL,
-                comment text,
+                patient_email varchar NOT NULL,
+                patient_comment varchar,
+                doctor_comment varchar ,
                 temperature REAL NOT NULL,
-                isClosed BOOLEAN NOT NULL CHECK (isClosed IN (0,1)),
-                hasCough BOOLEAN NOT NULL CHECK (hasCough IN (0,1)),
-                hasFever BOOLEAN NOT NULL CHECK (hasFever IN (0,1)),
-                hasShortnessOfBreath BOOLEAN NOT NULL CHECK (hasShortnessOfBreath IN (0,1)),
-                hasMusclePain BOOLEAN NOT NULL CHECK (hasMusclePain IN (0,1)),
-                hasSoreThroat BOOLEAN NOT NULL CHECK (hasSoreThroat IN (0,1)),
-                hasLossOfTasteOrSmell BOOLEAN NOT NULL CHECK (hasLossOfTasteOrSmell IN (0,1)),
-                hasContactWithCoronaCase BOOLEAN NOT NULL CHECK (hasContactWithCoronaCase IN (0,1))
+                is_closed BOOLEAN NOT NULL CHECK (is_closed IN (0,1)),
+                is_suspect BOOLEAN NOT NULL CHECK (is_suspect IN (0,1)),
+                is_positive BOOLEAN NOT NULL CHECK (is_positive IN (0,1)),
+                has_cough BOOLEAN NOT NULL CHECK (has_cough IN (0,1)),
+                has_fever BOOLEAN NOT NULL CHECK (has_fever IN (0,1)),
+                has_shortness_of_breath BOOLEAN NOT NULL CHECK (has_shortness_of_breath IN (0,1)),
+                has_muscle_pain BOOLEAN NOT NULL CHECK (has_muscle_pain IN (0,1)),
+                has_sore_throat BOOLEAN NOT NULL CHECK (has_sore_throat IN (0,1)),
+                has_loss_of_ts BOOLEAN NOT NULL CHECK (has_loss_of_ts IN (0,1)),
+                has_contact_with_coronac BOOLEAN NOT NULL CHECK (has_contact_with_coronac IN (0,1))
+                has_recommendation BOOLEAN CHECK (has_contact_with_coronac IN (0,1))
             );"""
     try:
         cur = conn.cursor()
@@ -152,59 +249,7 @@ def create_requests_table(conn):
         app.logger.error(e)
 
 
-class PatientRequest:
-
-    def __init__(self,
-                 patient_email,
-                 temperature,
-                 hasCough,
-                 hasFever,
-                 hasShortnessOfBreath,
-                 hasMusclePain,
-                 hasSoreThroat,
-                 hasLossOfTasteOrSmell,
-                 hasContactWithCoronaCase,
-                 isClosed=False,
-                 id=None,
-                 comment="",
-                 isNegative=False):
-        self.patient_email = patient_email
-        self.temperature = temperature
-        self.hasCough = hasCough
-        self.hasFever = hasFever
-        self.hasShortnessOfBreath = hasShortnessOfBreath
-        self.hasMusclePain = hasMusclePain
-        self.hasSoreThroat = hasSoreThroat
-        self.hasLossOfTasteOrSmell = hasLossOfTasteOrSmell
-        self.hasContactWithCoronaCase = hasContactWithCoronaCase
-        self.id = id
-        self.comment = comment
-        self.isClosed = isClosed
-        self.isNegative = isNegative
-
-    def __str__(self) -> str:
-        return super().__str__()
-
-    def is_corona_suspect(self):
-        if (self.temperature > 38) and  (self.hasCough or self.hasSoreThroat or self.hasMusclePain or self.hasShortnessOfBreath or self.hasContactWithCoronaCase or self.hasLossOfTasteOrSmell):
-            return True
-        if self.hasContactWithCoronaCase and (self.hasCough or self.hasSoreThroat or self.hasMusclePain or self.hasShortnessOfBreath or self.hasContactWithCoronaCase or self.hasLossOfTasteOrSmell or self.hasFever):
-            return True
-
-        positiveSuspections = 0
-
-        if self.hasFever: positiveSuspections += 1
-        if self.hasCough: positiveSuspections += 1
-        if self.hasShortnessOfBreath: positiveSuspections += 1
-        if self.hasMusclePain: positiveSuspections += 1
-        if self.hasSoreThroat: positiveSuspections += 1
-        if self.hasLossOfTasteOrSmell: positiveSuspections += 1
-
-        if positiveSuspections > 2:
-            return True
-
-        return False
-
+### MODEL
 
 
 
