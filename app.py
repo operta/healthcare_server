d6603d33c38c4458ee872edcfadff9e89ed3c95e
Dzan Operta <dzanoperta@yahoo.com>
2020-06-14 15:26:36 +0200

    deployed:

--- app.py
+++ app.py
@@ -169,11 +169,9 @@ def prescreening_request():
         if 'patientComment' in data:
             patient_request.patient_comment = data['patientComment']
 
-            
         if 'patient_comment' in data:
             patient_request.patient_comment = data['patient_comment']
            
-
         conn = connect_to_db()
         create_requests_table(conn)
         db.create_all()
